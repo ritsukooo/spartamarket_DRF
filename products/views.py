f1ae8ff9ce913fa48b72ab57fbf9b1bf9bddcba8
ritsukooo <chaeyeon5584@gmail.com>
2024-04-30 13:23:41 +0900

    상품 등록

--- products/views.py
+++ products/views.py
@@ -3,12 +3,24 @@ from rest_framework.views import APIView
 from .models import Product
 from rest_framework.response import Response
 from .serializers import ProductsSerializer
+from rest_framework import status
 
 
 # Create your views here.
 class ProductListAPIView(APIView):
-    def get(self,request):                                #-------------------상품목록 조회--------------------------#
+    def get(self,request):                                                #-------------------상품목록 조회--------------------------#
         produts = Product.objects.all()
         serializer = ProductsSerializer(produts, many=True)
         return Response(serializer.data)
+    
+    
+
+    def post(self,request):                                               #---------------------상품 등록-----------------------------#
+        serializer = ProductsSerializer(data =request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED) 
+        
+        
+        
         
