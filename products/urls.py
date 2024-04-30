9018ccaf9186cf19d1b12519e2f4e0153a9d1b4c
ritsukooo <chaeyeon5584@gmail.com>
2024-04-30 13:09:53 +0900

    상품 등록 기능 구현

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
+from . import views
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 
 app_name = "products"
 urlpatterns = [
+    path("", views.ProductListAPIView.as_view(), name="products_list"),
    
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductsSerializer
+
 
 # Create your views here.
+class ProductListAPIView(APIView):
+    def get(self,request):                                #-------------------상품목록 조회--------------------------#
+        produts = Product.objects.all()
+        serializer = ProductsSerializer(produts, many=True)
+        return Response(serializer.data)
+        
